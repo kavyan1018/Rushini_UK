8f4a29368b854ef8cb8a9c45159f8e633b51e3b8
Kavyan Nayak <kavyannayak104@gmail.com>
2024-08-29 14:14:23 +0530

    first commit

---
+++ 1.py
@@ -0,0 +1,30 @@
+# Features of Python
+
+# 1. Python is a high-level language.
+# 2. Python is an interpreted language.
+# 3. Python is an object-oriented language.
+# 4. Python is a dynamically typed language. --> We do not need to specify the datatype of avariable during writing codes
+# 5. Python is a platform-independent language.
+# 6. Python is a simple language.
+# 7. Python is a free and open-source language.
+# 8. Python is a portable language.  --> We can run python code on any platform or any machine or any operating System.
+# 9. Python have a Very large standard library.
+# 10. Python is a case-sensitive language. --> Python is case-sensitive language. It means that the variable name "a" and "A" are two different variables in python.
+# 11. Python is a beginner's language. --> Python is a beginner's language. It means that it is easy to learn and understand.
+
+
+# Python is used in the following fields: / # Where is Pyhton used?  
+
+# Data Science
+# Machine Learning / AI
+# Web Development (Django, Flask)
+# Mobile Application Development
+# Desktop Application Development
+
+# What is Desktop Application Development?
+# Desktop Application Development is the process of creating standalone software applications that run on a user's local computer.
+
+
+# Python is not used in Game Development. For Game Development, we use C++ or Java or C#.
+# Python is not used in System Programming. For System Programming, we use C or C++.
+# Python is not used in Embedded Programming. For Embedded Programming, we use C or C++.
