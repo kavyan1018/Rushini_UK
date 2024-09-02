771df953bd9123c97ef0f292ccaf038c3093a665
Kavyan Nayak <kavyannayak104@gmail.com>
2024-09-02 14:23:29 +0530

    First Print

---
+++ first.py
@@ -0,0 +1,48 @@
+print('Hello World') 
+# How are you?
+# How are you?
+
+# in-line comment / Single line comment
+"""
+Multi 
+Line 
+Comment
+"""
+'''
+Comment
+using 
+Singlr Quotes
+'''
+
+print("How are you?") # print statement of python ends with '\n' by default
+
+'''
+My name is <Your Name>!
+
+
+'''
+
+
+"""
+    srNo   Name    Age     City
+"""
+
+# \t --> tab
+
+print("srNo\tName\tAge\tCity")
+print("1\tAli\t22\tKarachi")
+print("2\tAhmed\t23\tLahore")  
+print("3\tAsad\t24\tIslamabad")
+
+
+
+# print the following messages on the screen as they appear:
+
+"""
+Alpha Said Beta "Gamma is teasing Delta."
+
+Alpha Said Beta "Gamma is teasing Delta but Gamma says he didn't tease  Delta."
+
+The location of Python interpreter is: "C:\program files\new folder\temp\python\"
+"""
+
