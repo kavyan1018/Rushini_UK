de571757b12349191c8d319c48f450678c82bc25
Kavyan Nayak <kavyannayak104@gmail.com>
2024-09-03 15:39:15 +0530

    Backspace

---
+++ Second.py
@@ -0,0 +1,26 @@
+# print('The location of Python interpreter is: "C:\\program files\\new folder\\temp\\python\\"')
+
+
+# Escape Sequences Characters : \, \b => backspace, \n => new line, \t => tab, 
+
+
+# print("Python is fun\b\b Awesome!\b", end="")   # python print statement ends with '\n' by default. 
+
+# print("Python is fun\b\b\b Awesome!", end=" ")
+
+# print("Python is fun\b\b\b\b Awesome!", end="\t")
+
+# print("Python is fun\b\b\b\b\b Awesome!", end="\n")
+
+# print("Python is fun\b\b\b Awesome!", end="Jay")
+
+# print("Python is fun\b\b\b Awesome!", end="\b")
+
+print("Python is fun and Awesome!", end="")
+
+print("I Love Python Programing", end="\b\b\b\b\b\b")
+
+print("Python is fun Awesome!", end="\n")
+
+# print("Python is Fun\b\b\bR")
+
