b7d57ad4f96de81ea7ec33adc3f984af35d8f00e
Kavyan Nayak <kavyannayak104@gmail.com>
2024-09-07 22:01:07 +0530

    Complex Number

---
+++ complexNum.py
@@ -0,0 +1,60 @@
+"""
+squre root of 16 = > 4 and (-4)
+
+beacuse 4*4 = 16 
+and 
+-4*-4 = 16
+
+
+squre root of 25 => 5 and (-5)
+
+beacuse 5*5 = 25
+and
+(-5) * (-5) = 25
+
+
+squre root of 49 => 7
+squre root of 49 => (-7)
+
+squre root of -36 => 6
+(-6) * (-6) = -36
+
+sqrt of (-3) = sqrt of (36 * (-1))
+             = sqrt of (36) * sqrt of (-1)
+             = 6j where j = sqrt of (-1) = i
+
+"""
+
+# Complex Number Data Type
+
+# Example 1: Complex Number Data Type
+
+a = 5j
+print(a)
+
+b = 4j
+print(b)
+
+print(a*b)
+
+"""
+sqrt(3) * sqrt(3) = sqrt(9) = 3 or -3
+sqrt(-2) * sqrt(-2) = sqrt(4) = 2 or -2
+sqrrt(-1) * sqrt(-1) = 1 or -1
+j * j = 1 or -1
+
+
+a * b = 5j * 4j = 20 * 1 = 20
+                = 20 * (-1) = -20
+"""
+
+
+c = 3j + 7
+d = 4j - 5
+
+print(c * d)
+
+"""
+(3j + 7) (4j - 5) = 
+"""
+
