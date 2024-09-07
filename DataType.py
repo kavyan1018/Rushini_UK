dce8241673c702ce50fb140147f1e479054b16ad
Kavyan Nayak <kavyannayak104@gmail.com>
2024-09-07 21:36:23 +0530

    Data Types

---
+++ DataType.py
@@ -0,0 +1,27 @@
+# How many types of data are there in Python?
+
+# Answer: There are 4 types of data in Python. They are:
+
+# 1. Numeric Data Type
+# 2. Sequence Data Type
+# 3. Set Data Type
+# 4. Mapping Data Type
+
+# Numeric Data Type: int, float, complex, boolean
+
+# Sequence Data Type: str, list, tuple
+
+# Set Data Type: set, frozenset
+
+# Mapping Data Type: dict
+
+
+# Numeric Data Type: int, float, complex, boolean
+
+# integer =>  Numeric value (+ve or -ve) 
+
+# float => Numeric value with decimal point (0.0)
+
+# complex => Numeric value with real and imaginary part (a+bj)
+
+# boolean => True or False
---
+++ NumericData.py
@@ -0,0 +1,35 @@
+# Interger Data Type
+
+# # Example 1: Integer Data Type
+
+x = 10
+print(x)
+
+# # Example 2: Integer Data Type
+
+y = -10
+print(y)
+
+# # Example 3: Integer Data Type
+
+z = 0
+print(z)
+
+
+# Float Data Type
+
+# # Example 1: Float Data Type
+
+a = 1.5
+print(a)
+
+# # Example 2: Float Data Type
+
+b = -1.5
+print(b)
+
+# # Example 3: Float Data Type
+
+x = 0.5
+print(x)
+
