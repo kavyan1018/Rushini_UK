fb956007762ec4abcccdff35a2d9c31e0be088c1
Kavyan Nayak <kavyannayak104@gmail.com>
2024-09-08 21:43:30 +0530

    Arithmetic Operators

---
+++ oprators.py
@@ -0,0 +1,51 @@
+# types of operators in python
+
+# 1. Arithmetic Operators
+# 2. Assignment Operators
+# 3. Comparison Operators
+# 4. Logical Operators
+
+# Arithmetic Operators
+
+# 1. Addition
+# 2. Subtraction
+# 3. Multiplication
+# 4. Division
+# 5. Modulus
+
+# Example 1: Addition
+
+
+'''a = 10
+b = 5
+c = a + b
+print("Addition is :- ",c)'''
+
+# Example 2: Subtraction
+
+'''x = 5
+y = 10
+z = x - y
+
+print("Subtraction is :- ",z)'''
+
+# Example 3: Multiplication
+
+'''a = 10
+b = 5
+c = a * b
+print("Multiplication is :- ",c)'''
+
+# Example 4: Division
+
+'''c = 10
+d = 5
+e = c / d
+print("Division is :- ",e)'''
+
+# Example 5: Modulus
+
+a = 10
+b = 5
+c = a % b
+print("Modulus is :- ",c)
