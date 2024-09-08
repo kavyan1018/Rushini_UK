2dabc86eae33638fbdea936a84490095d41341d2
Kavyan Nayak <kavyannayak104@gmail.com>
2024-09-08 21:28:46 +0530

    String

---
+++ str.py
@@ -0,0 +1,44 @@
+# how many types of keywords are there in python?
+# 1. 33 keywords
+
+
+# takeing input from user
+"""
+print("Enter the number a :")
+a = input()
+# print(a)
+print("a =",a)
+
+b = input("Enter the number b :")
+print("b =",b)
+
+a = int(a)
+b = float(b)
+c = a + b
+
+print("sum = ",c)
+
+print("types of a : ",type(a))
+print("types of b : ",type(b))
+
+"""
+"""
+p = "abc"
+q = "xyz"
+
+print(p + q)
+
+r = "13" # if we write any number, alphabet, float  value etc... in double quotes then it will be considered as string
+print("type of r :",type(r))
+print("type of p :",type(p))
+"""
+
+
+a = "10.3"
+print("type of a :",type(a))
+
+b = 10.3
+print("type of b :",type(b))
+
+c  = 10
+print("type of c :",type(c))
