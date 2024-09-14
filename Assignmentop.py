19be8423db7780e29bd000b661dc488f8d4cef45
Kavyan Nayak <kavyannayak104@gmail.com>
2024-09-14 21:44:42 +0530

    Assignment_operators

---
+++ Assignmentop.py
@@ -0,0 +1,83 @@
+# What is Assignment oprator?
+# Assignment operators are used in Python to assign values to variables.
+
+# x,a,b,c,z
+
+# x = 6
+# print(type(x))
+
+# There are 8 assignment operators in Python:
+
+# 1. = (Assignment Operator)
+# 2. += (Addition Assignment)
+# 3. -= (Subtraction Assignment)
+# 4. *= (Multiplication Assignment)
+# 5. /= (Division Assignment)
+# 6. %= (Modulus Assignment)
+# 7. //= (Floor Division Assignment)
+# 8. **= (Exponentiation Assignment)
+
+# = (Assignment Operator)
+
+# x = 5
+# print(x)
+
+# += (Addition Assignment)
+
+# x = 2
+# x += 5
+# print(x)
+
+# 3. -= (Subtraction Assignment)
+
+# y = 10
+# # y -= 2
+# y = y - 2
+
+# print(y)
+
+"""
+    x = 10
+    y ?
+
+    y = x - 2 
+"""
+
+# 4. *= (Multiplication Assignment)
+
+# x = 5
+# x *= 2
+# x = x * 2
+
+# print(x)
+
+# 5. /= (Division Assignment) Division Assignment is return the float value
+
+# x = 10
+# x /= 2
+# x = x / 2
+
+# print(x)
+
+# 6. %= (Modulus Assignment)
+
+# x = 10
+# x %= 2  
+# x = x % 2
+
+# print(x)
+
+# 7. //= (Floor Division Assignment) floor division is retuen the integer value
+
+# x = 10
+# x //= 3
+# x = x // 3
+
+# print(x)
+
+# 8. **= (Exponentiation Assignment)
+
+x = 2
+x **= 3
+
+print(x)
