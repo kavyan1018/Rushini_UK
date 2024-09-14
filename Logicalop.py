e30021dd8af8fee205e2657c0a92027cb60da67c
Kavyan Nayak <kavyannayak104@gmail.com>
2024-09-14 21:53:11 +0530

    Logicalop_operators

---
+++ Logicalop.py
@@ -0,0 +1,33 @@
+# What is logical operator?
+# Logical operators are the and, or, not operators.
+
+
+# Example 1: Logical AND
+
+# a = 10
+# b = 20
+
+# if a > 5 and b > 10:
+#     print("Both conditions are True")
+# else:
+#     print("Both conditions are not True")
+
+# Example 2: Logical OR
+
+# a = 10  
+# b = 20
+
+# if a > 5 or b > 10:
+#     print("At least one of the conditions is True")
+# else:
+#     print("Both conditions are not True")
+
+# Example 3: Logical NOT
+
+a = 10  
+b = 20
+
+if not a > 5:
+    print("a is not greater than 5")
+else:
+    print("a is greater than 5")
