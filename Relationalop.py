47a8fc40a0f2bc1ec411573e5788c3197ddd25f0
Kavyan Nayak <kavyannayak104@gmail.com>
2024-09-14 21:16:34 +0530

    Relational operators

---
+++ Relationalop.py
@@ -0,0 +1,80 @@
+# what is Relational oprators in python?
+# Relational operators are used to compare two values.
+
+# There are 6 relational operators in Python:
+
+# 1. Less than (<)
+# 2. Less than or equal to (<=)
+# 3. Greater than (>)
+# 4. Greater than or equal to (>=)
+# 5. Equal to (==)
+# 6. Not equal to (!=)
+
+
+# what is the difference between == and = ?
+# = is an assignment operator
+# x = 5
+
+# == is a relational operator
+# 5 == 5: to comper two values
+
+# Example 1: Less than (<)
+
+# a = 10
+# b = 5
+
+# if a < b:
+#     print("a is less than b")
+# else:
+#     print("a is not less than b")
+
+# Example 2: Less than or equal to (<=)
+
+# a = 10
+# b = 10
+
+# if a <= b:
+#     print("a is less than b")
+# else:
+#     print("a is not less than b")
+
+# Example 3: Greater than (>)
+
+# a = 10
+# b = 5
+
+# if a > b:
+#     print("a is greater than b")
+# else:
+#     print("a is not greater than b")
+
+# Example 4: Greater than or equal to (>=)
+
+# a = 10
+# b = 10
+
+# if a >= b:
+#     print("a is greater than b")
+# else:
+#     print("a is not greater than b")
+
+
+# Example 5: Equal to (==)
+
+# a = 10
+# b = 1
+
+# if a == b:
+#     print("a is equal to b")
+# else:
+#     print("a is not equal to b")
+
+# Example 6: Not equal to (!=)
+
+# a = 10
+# b = 5
+
+# if a != b:
+#     print("a and b are not equal")
+# else:
+#     print("a and b are equal")
