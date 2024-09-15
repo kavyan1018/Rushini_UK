ed5143a573921751e184c2ff9be223620b0a358b
Kavyan Nayak <kavyannayak104@gmail.com>
2024-09-15 22:02:29 +0530

    Example

---
+++ ifelse/Examples.py
@@ -0,0 +1,8 @@
+# take input from usear check alphabet is vowel or consonant ? (capital)
+
+ch = input("Enter a character: ")
+
+if ch == "A" or ch == "E" or ch == "I" or ch == "O" or ch == "U" or ch == "a" or ch == "e" or ch == "i" or ch == "o" or ch == "u":
+    print("The Character is a Vowel")
+else:
+    print("The Character is a Consonant")
