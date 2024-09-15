1425796208e67cd783e696a6cdd5b79c0e95e11a
Kavyan Nayak <kavyannayak104@gmail.com>
2024-09-15 21:30:13 +0530

    If_Else

---
+++ ifelse/ifelse.py
@@ -0,0 +1,49 @@
+# What is if else in python?
+# The if else statement is used to execute a block of code whether the condition is True or False.
+
+
+# ----------------------------------------
+
+# Syntax:
+
+# if condition:
+#   statement
+# else:
+#   statement
+
+
+# -----------------------------------------
+# Example 1
+
+# a = 10
+# b = 20
+
+
+# # condition:
+# if a > b:
+#     print("A is Max")
+# else:
+#     print("B is Max")
+
+
+# Example 2 : i want to take input from user
+
+# a = int(input("Enter the value of a: "))
+# b = int(input("Enter the value of b: "))
+
+# if a > b:
+#     print("A is Max")
+# else:
+#     print("B is Max")
+
+
+
+
+# check number is even or odd?
+
+num = int(input("Enter the number: "))
+
+if num % 2 == 0:
+    print("Number is Even")
+else:
+    print("Number is Odd")
