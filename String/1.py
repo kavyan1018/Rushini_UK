b6414dfa3d1ab0587b39a2fc68c5c44716f9cbc4
Kavyan Nayak <kavyannayak104@gmail.com>
2024-10-05 15:21:34 +0530

    String

---
+++ String/1.py
@@ -0,0 +1,28 @@
+# mutablity and immutablitity
+# python straing is immutable
+# String is an iterable object
+# String is subscriptable
+
+# find index of a character in a string
+# name = "Amit Thaakar"
+# print(name[0])   # A
+# print(name[1])  # m
+# print(name[2])  # i
+# print(name[5])
+
+
+# find the length of a string
+
+# l = len(name)
+# print(l)
+
+
+# l = len(name)
+# print("len",l)
+
+
+# take input from user and print the length of the string
+
+name = input("Enter your name: ")
+l = len(name)
+print("Length of the string is: ",l)
