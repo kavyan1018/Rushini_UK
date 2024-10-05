dc27842d32acdbfd16adf4f138f2b397546f4d92
Kavyan Nayak <kavyannayak104@gmail.com>
2024-10-05 16:03:00 +0530

    String

--- String/3.py
+++ String/3.py
@@ -1,5 +1,5 @@
 # lower TO UPPER CASE WITHOUT USING UPPER FUNCTION
-
+"""
 name = input("Enter your name: ")
 upper = ""
 
@@ -7,6 +7,7 @@ upper = ""
 
 for i in name:
     if ord(i) >= 97 and ord(i) <= 122:
+        # "" = "" + char (97) - 32  kavyan
         upper = upper + chr(ord(i)-32)
 
 print(upper)
@@ -16,3 +17,25 @@ print(chr(x)) # A
 
 x = "A"
 print(ord(x))
+
+"""
+
+
+# k = 107  - 32 =>    75
+# a = 97   - 32 =>    65
+# v = 118  - 32 =>    86
+# y = 121  - 32 =>    89 
+# a = 97   - 32 =>    65
+# n = 110  - 32 =>    78
+
+
+# UPPER TO LOWER CASE WITHOUT USING LOWER FUNCTION
+name = input("Enter your name: ")
+lower = ""
+
+for i in name:
+    if ord(i) >= 65 and ord(i) <= 90:
+        # "" = "" + char (97) - 32  kavyan
+        lower = lower + chr(ord(i) + 32)
+
+print(lower)
