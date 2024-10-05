004d0053a3fd09f9613e8e5a045a4580277a7879
Kavyan Nayak <kavyannayak104@gmail.com>
2024-10-05 15:31:44 +0530

    Ascii_Table

---
+++ String/2.py
@@ -0,0 +1,21 @@
+# content: string concatenation 
+name = "Ram"
+# name = "name + laxman"
+# name += " laxman"
+# print(name)
+
+# name += ",laxman"
+# print(name)
+
+# Upper case
+name = name.upper()
+print(name)
+
+# Lower case
+
+name = name.lower()
+print(name)
+
+
+x = 65
+print(chr(x))
