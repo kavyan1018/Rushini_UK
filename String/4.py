ce4f87234cca1a3a444fe697bef77c631e2f9e8d
Kavyan Nayak <kavyannayak104@gmail.com>
2024-10-06 15:29:58 +0530

    String

---
+++ String/4.py
@@ -0,0 +1,43 @@
+"""
+# Title method: String.title()
+
+# Title method is used to convert the first character of each word to uppercase and the rest of the characters to lowercase.
+
+# Example:
+
+str = "hello, my name is john"
+print(str.title())
+
+"""
+
+
+
+
+"""
+
+# capitalize : String.capitalize()
+
+# The capitalize() method is used to convert the first character of the string to uppercase and the rest of the characters to lowercase.
+
+# Example:
+
+
+a = "hii My name is kavyan"
+
+print(a.capitalize())
+
+"""
+
+
+"""
+# swapcase : String.swapcase()
+
+# The swapcase() method is used to convert the uppercase characters to lowercase and the lowercase characters to uppercase.
+
+# Example:
+
+a = "hii My name is Kavyan"
+print(a.swapcase())
+
+"""
+
