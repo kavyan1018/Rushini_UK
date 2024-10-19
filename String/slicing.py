a6fdcca14e2c218a80fdbb1216450329600f2c43
Kavyan Nayak <kavyannayak104@gmail.com>
2024-10-19 21:43:36 +0530

    Sclicing

---
+++ String/slicing.py
@@ -0,0 +1,8 @@
+name = "ahmedabad"
+x = name[ : : ]    #ahmedeaba
+# print(x)
+# print(name[0::])
+# print(name[0::2])      #it will return only odd char from name   or it will return 0,2,4,6,8
+# print(name[0:4:2])
+# print(name[::-1])
+print(name[::-2])   
