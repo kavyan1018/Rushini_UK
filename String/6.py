6313ac7b4c567f9e569cd83063193898e4ad53e0
Kavyan Nayak <kavyannayak104@gmail.com>
2024-10-19 21:30:32 +0530

    Inbult Methods

---
+++ String/6.py
@@ -0,0 +1,110 @@
+# startswith
+# endswith
+# find
+# index
+# isPrintable
+# strip
+# lstrip
+# rstrip
+
+"""
+# startswith
+# Example :-
+str = "Hello, World!"
+
+x = str.startswith("h")
+
+print(x)
+"""
+
+# --------------------------------------------------------------------------------
+"""
+# endswith
+
+# Example:-
+str = "Hello, World!"
+x = str.endswith("!")
+print(x)
+"""
+
+# --------------------------------------------------------------------------------
+
+"""
+# find
+# find() method returns the index of the first occurrence of the substring (if found). If not found, it returns -1.
+# Syntax: str.find(substring, start, end)
+# Example:
+str = "Hello, World!"
+x = str.find("E")
+print(x)
+"""
+
+# --------------------------------------------------------------------------------
+"""
+# index
+# index() method returns the index of the first occurrence of the substring (if found). If not found, it raises an exception.
+
+# Syntax: str.index(substring, start, end)
+# Example:
+
+str = "Hello, World!"
+x = str.index("e")
+print(x)
+"""
+
+# --------------------------------------------------------------------------------  
+"""
+# isPrintable
+# isPrintable() method returns True if all the characters are printable.
+
+# Syntax: str.isprintable()
+# Example:
+
+str = "23321"
+x = str.isprintable()
+print(x)
+"""
+
+
+# --------------------------------------------------------------------------------
+
+# strip
+# lstrip
+# rstrip
+
+"""
+# strip() method removes any leading (spaces at the beginning) and trailing (spaces at the end) characters (space is the default leading character to remove)
+
+# Syntax: str.strip(characters)
+# Example:
+
+str = " Hello, World! "
+x = str.strip()
+print(x)
+"""
+
+# --------------------------------------------------------------------------------
+"""
+# lstrip
+
+# Syntax : str.lstrip(characters)
+# Example:-
+
+str = " Hello, World! "
+x = str.lstrip()
+print(x)
+"""
+
+
+# --------------------------------------------------------------------------------
+
+# """
+# rstrip
+
+# Syntax: str.rstrip(characters)
+# Example:
+
+str = " Hello, World! "
+x = str.rstrip()
+print(x)
+# """
