5d96b61816ec9c1b849c6a301234ecf1277b0030
Kavyan Nayak <kavyannayak104@gmail.com>
2024-10-19 21:49:41 +0530

    Slicing

--- String/slicing.py
+++ String/slicing.py
@@ -6,3 +6,9 @@ x = name[ : : ]    #ahmedeaba
 # print(name[0:4:2])
 # print(name[::-1])
 print(name[::-2])   
+
+
+# HOME WORK 
+
+my_list = [10, 20, 30, 40, 50, 60]
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
