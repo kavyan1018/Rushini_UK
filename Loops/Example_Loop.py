64863dfc5633453f0bc3a645d93818634276455c
Kavyan Nayak <kavyannayak104@gmail.com>
2024-10-23 23:22:03 +0530

    Examples of Loops

---
+++ Loops/Example_Loop.py
@@ -0,0 +1,20 @@
+# print all even numbers from 1 to 50 using for loop
+# there is total 2 ways
+
+# 1st way :  only use for loop
+
+# for i in range(0, 51, 2):
+#     print(i)
+
+
+# 2nd way :  use for loop and if condition
+
+
+# for i in range(1, 51):
+#     if i % 2 == 0:
+#         print(i)
+
+
+# print all odd numbers from 1 to 50 using for loop
+# 1st way :  only use for loop
+# 2nd way :  use for loop and if condition
