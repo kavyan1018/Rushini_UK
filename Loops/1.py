7e3fa85cb5aa41a1cc6ca225b746740cc38e1566
Kavyan Nayak <kavyannayak104@gmail.com>
2024-10-23 22:48:01 +0530

    Loops

---
+++ Loops/1.py
@@ -0,0 +1,19 @@
+#  for i in range(10) :  it will start form 0 and end at 9
+
+# for i in range(1,16)  :   it will start from 1 and end at 15
+
+# i = 0;  -->  initialization(Stating point)
+# i < 5;  -->  condition (End of loop)
+# i++;    -->  increment/decrement (Step)
+
+# (initialization, condition, increment/decrement)
+#  for i in range(i=1, i < 16, 2(i++/i--)) : it will start from 1 , end at 15 and increment by 2
+
+# for i in range(1,16) :
+#     print(i)
+
+
+i = "HELLO WORLD"
+
+for i in range(5):
+    print(i)
