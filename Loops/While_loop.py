8b460ef25b847f3e3f1281351de62ef83afbc24a
Kavyan Nayak <kavyannayak104@gmail.com>
2024-10-26 19:55:06 +0530

    While_loop

---
+++ Loops/While_loop.py
@@ -0,0 +1,22 @@
+# syntax of while loop
+
+# while condition:
+#     statement1
+
+# while condition:
+#     statement1
+
+# i = 1
+# while i <= 5:
+#     print(i)
+#     i += 1
+
+    # satrt  1 2 3 4 
+
+
+# even number
+
+i = 0
+while i <= 50:
+    print(i)
+    i += 2
