9d6615f6d836ff1490915decb437b21e0641bbe5
Kavyan Nayak <kavyannayak104@gmail.com>
2024-10-26 19:56:14 +0530

    While_loop

--- Loops/While_loop.py
+++ Loops/While_loop.py
@@ -15,8 +15,12 @@
 
 
 # even number
-
+# 1st way
 i = 0
 while i <= 50:
     print(i)
     i += 2
+
+
+# even number 2nd way(using if condition)
+# odd number 1st (whithout using if condition) and 2nd way (using if condition)
