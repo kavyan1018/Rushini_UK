4f3047bc5cbb30475e6d8749debec88ebb5ae5d8
Kavyan Nayak <kavyannayak104@gmail.com>
2024-10-26 19:28:50 +0530

    Example

--- Loops/Example_Loop.py
+++ Loops/Example_Loop.py
@@ -16,5 +16,13 @@
 
 
 # print all odd numbers from 1 to 50 using for loop
-# 1st way :  only use for loop
-# 2nd way :  use for loop and if condition
+# 1st way :  only use for loop 
+# 2nd way :  use for loop and if conditionNaan Na Naan Ord Number Outputs. 
+
+
+
+# output : 1,3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35, 37, 39, 41, 43, 45, 47, 49
+
+
+for i in range(1, 50, 2):
+    print(i)
