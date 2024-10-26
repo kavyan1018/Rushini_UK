5afefd9da295377cd3c319bfe94282bc93915e56
Kavyan Nayak <kavyannayak104@gmail.com>
2024-10-26 19:46:39 +0530

    Example

--- Loops/Example_Loop.py
+++ Loops/Example_Loop.py
@@ -24,5 +24,10 @@
 # output : 1,3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35, 37, 39, 41, 43, 45, 47, 49
 
 
-for i in range(1, 50, 2):
-    print(i)
+# for i in range(1, 50, 2):
+#     print(i)
+
+
+# for i in range(1, 51):
+#     if i % 2 == 1:
+#         print(i)    
