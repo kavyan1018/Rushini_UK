26dd144fa921cc3860bb9cb13e5d2e05225f8760
Kavyan Nayak <kavyannayak104@gmail.com>
2024-10-27 15:08:26 +0530

    While loop example

--- Loops/Whileloop_Example.py
+++ Loops/Whileloop_Example.py
@@ -2,3 +2,10 @@
 # output : 55
 # natural number : 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
 
+sum = 0
+for i in range(1, 11):
+    sum += i
+print("sum of Natural Numbers are :" ,sum)  
+
+
+# Home work : print sum of first 10 natural number using while loop
