264ca32b1e8ecfe583fbc92647a038d83501987d
Kavyan Nayak <kavyannayak104@gmail.com>
2024-10-29 16:34:25 +0530

    Example

--- Loops/Examples of loop.py
+++ Loops/Examples of loop.py
@@ -4,12 +4,21 @@
 # output : 9 8 7 6 5 4 3 2 
 
 
-a = int(input("Enter Starting number : "))
-b = int(input("Enter Ending number : "))
+# a = int(input("Enter Starting number : "))
+# b = int(input("Enter Ending number : "))
 
-for i in range(a, b - 1, -1):
-    print(i)
+# for i in range(a, b - 1, -1):
+#     print(i)
 
 # output : 9 8 7 6 5 4 3 2
 
 
+# while loop 
+
+a = int(input("Enter Starting number : "))
+b = int(input("Enter Ending number : "))
+
+i = a    # initialization
+while i >= b:  # condition
+    print(i)   # statement
+    i -= 1   # increment/decrement
