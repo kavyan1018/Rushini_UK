8bf68795962030c9277227d3b3cec9567a35c677
Kavyan Nayak <kavyannayak104@gmail.com>
2024-10-30 16:25:07 +0530

    Example

--- Loops/Examples of loop.py
+++ Loops/Examples of loop.py
@@ -15,10 +15,45 @@
 
 # while loop 
 
-a = int(input("Enter Starting number : "))
-b = int(input("Enter Ending number : "))
+# a = int(input("Enter Starting number : "))
+# b = int(input("Enter Ending number : "))
 
-i = a    # initialization
-while i >= b:  # condition
-    print(i)   # statement
-    i -= 1   # increment/decrement
+# i = a    # initialization
+# while i >= b:  # condition
+#     print(i)   # statement
+#     i -= 1   # increment/decrement
+
+
+# palindrome number
+# user input => 1221   reversed 1221 => this is a palindrome number
+
+
+
+# num = int(input("Enter the number :"))
+# temp = num
+# reversed_number = 0
+
+# while num > 0:
+#     rem = num % 10
+#     reversed_number = (reversed_number * 10) + rem
+#     num = num // 10
+
+# if temp == reversed_number:
+#     print("This is a palindrome number")
+# else:   
+#     print("This is not a palindrome number")
+    
+
+
+# Find first and last digit of number;  ex. 159643 -> first=1  , last=3
+
+num = int(input("Enter the number :"))
+first = num
+
+while first >= 10:
+    first = first // 10
+
+last = num % 10
+
+print("First digit is : ", first)
+print("Last digit is : ", last)
