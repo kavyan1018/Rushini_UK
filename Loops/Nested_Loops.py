51fdf27018883cd836c988ac5defb825952f61f6
Kavyan Nayak <kavyannayak104@gmail.com>
2024-10-30 16:32:38 +0530

    Nested Loop

---
+++ Loops/Nested_Loops.py
@@ -0,0 +1,13 @@
+# Nested Loop in python 
+# Nested loop is a loop inside a loop.
+
+# Syntax:
+for i in range(1, 5):
+    for j in range(1, 5):
+          print(i, j)
+
+# Output:
+# 1 1
+# 1 2
+# 1 3
+# 1 4
