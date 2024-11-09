91f836489f164d2b563a501466ddf74995734111
Kavyan Nayak <kavyannayak104@gmail.com>
2024-11-09 21:45:13 +0530

    ds

--- Loops/Nested_Loops.py
+++ Loops/Nested_Loops.py
@@ -16,10 +16,25 @@
 # While loop:
 
 
-i = 1
-while i <= 4:
-    j = 1
-    while j <= 4:
-        print(i, j)
-        j += 1
-    i += 1
+# i = 1
+# while i <= 4:
+#     j = 1
+#     while j <= 4:
+#         print(i, j)
+#         j += 1
+#     i += 1
+
+
+# pattern
+# *
+# **
+# ***
+# ****
+# *****
+
+
+for i in range(1, 6):
+    for j in range(1, i + 1):
+        print("*", end="")
+    print()
+    
---
+++ ds/1.py
@@ -0,0 +1,12 @@
+# Understand the concept of data structures in python
+# 1. Array
+# 2. records
+
+
+# Array in python
+# Array is a collection of items stored at contiguous memory locations.
+# The idea is to store multiple items of the same type together.
+# This makes it easier to calculate the position of each element by simply adding an offset to a base value, i.e., the memory location of the first element of the array (generally denoted by the name of the array).
+#   Array can be handled in python by a module named array. They can be useful when we have to manipulate only a specific data type values.
+#   A user can treat lists as arrays. However, user cannot constraint the type of elements stored in a list. If you create arrays using the array module, all elements of the array must be of the same type.
+
